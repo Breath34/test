9d8570a4bdc24cc95d8c0524dff074cd9b4faa4c
Breath34 <ohboi32q@gmail.com>
2025-05-13 17:09:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-while [ true ]; do
-    java -Xms6144M -Xmx6144M --add-modules=jdk.incubator.vector -jar server.jar --nogui
-
-    echo Server restarting...
-    echo Press CTRL + C to stop.
-done
+java -Xms6144M -Xmx6144M --add-modules=jdk.incubator.vector -jar server.jar --nogui
